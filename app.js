6ec1e2af8ac4e2309f03a77dbec1fb6618711cff
hooran-gallery <q1297504@gmail.com>
2026-06-17 10:08:15 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,74 +1,31 @@
-async function register() {
+const socket =
+io("https://bak-mf27.onrender.com");
 
-    const username =
-        document.getElementById("username").value;
-
-    const password =
-        document.getElementById("password").value;
-
-    if (!username || !password) {
-
-        document.getElementById("result")
-            .innerHTML =
-            "نام کاربری و رمز را وارد کنید";
-
-        return;
-    }
-
-    try {
-
-        const response = await fetch(
-            "https://bak-mf27.onrender.com/register",
-            {
-                method: "POST",
-
-                headers: {
-                    "Content-Type": "application/json"
-                },
-
-                body: JSON.stringify({
-                    username,
-                    password
-                })
-            }
-        );
-
-        const data = await response.json();
-
-        document.getElementById("result")
-            .innerHTML =
-            data.message;
-
-    } catch (err) {
-
-        document.getElementById("result")
-            .innerHTML =
-            "خطا در ارتباط با سرور";
-
-        console.log(err);
-    }
-}
-
-// ارسال پیام
-function sendMessage() {
+function sendMessage(){
 
     const text =
-        document.getElementById("message").value;
+    document.getElementById("message").value;
 
-    if (!text) return;
+    if(!text) return;
 
-    document.getElementById("messages")
+    socket.emit(
+        "send-message",
+        text
+    );
+
+    document.getElementById("message").value="";
+}
+
+socket.on(
+    "new-message",
+    (msg)=>{
+
+        document.getElementById("messages")
         .innerHTML +=
         `
-        <div style="
-            background:#eee;
-            padding:10px;
-            margin:10px;
-            border-radius:10px;
-        ">
-            ${text}
+        <div class="msg">
+            ${msg}
         </div>
         `;
-
-    document.getElementById("message").value = "";
-}
+    }
+);
